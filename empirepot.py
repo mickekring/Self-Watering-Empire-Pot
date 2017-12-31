ec7476b1315df00f40a86b71d7c8551a2d11797e
Micke Kring <Micke@Mickes-MacBook-Pro.local>
2017-12-31 10:25:18 +0100

    Try - Except
    Added Try - Except if Internet connection is down, so that the program
    will pass and keep running.

--- empirepot.py
+++ empirepot.py
@@ -103,18 +103,25 @@ def water_reading():
 	waterLevel = 0
 	global lastWatered
 	Thread(target = led_rolling).start()
-	tts = gTTS(text="Alert! Soil moisture levels will we tested in T minus two seconds." , lang='en')
-	tts.save("moisture.mp3")
-	os.system("mpg321 -q moisture.mp3")
+	try:
+		tts = gTTS(text="Alert! Soil moisture levels will we tested in T minus two seconds." , lang='en')
+		tts.save("moisture.mp3")
+		os.system("mpg321 -q moisture.mp3")
+	except:
+		os.system("mpg321 -q moisture.mp3")
+		pass
 	vattenbehov = 0
 	GPIO.output(hygro_Power, True)
 	time.sleep(1)
 	for x in range(1,4):
 		vattenbehov += GPIO.input(hygro)
 		print(vattenbehov)
-		tts = gTTS(text="Test {0} of three.".format(x) , lang='en')
-		tts.save("test.mp3")
-		os.system("mpg321 -q test.mp3")
+		try:
+			tts = gTTS(text="Test {0} of three.".format(x) , lang='en')
+			tts.save("test.mp3")
+			os.system("mpg321 -q test.mp3")
+		except:
+			pass
 		time.sleep(2)
 	GPIO.output(hygro_Power, False)
 	ledSwitch = 0
@@ -123,9 +130,13 @@ def water_reading():
 		ledSwitch = 1
 		waterLevel = 0
 		Thread(target = led_green_alert).start()
-		tts = gTTS(text="Code green. We have a code green. All systems stabilized and functioning within normal parameters." , lang='en')
-		tts.save("green.mp3")
-		os.system("mpg321 -q green.mp3")
+		try:
+			tts = gTTS(text="Code green. We have a code green. All systems stabilized and functioning within normal parameters." , lang='en')
+			tts.save("green.mp3")
+			os.system("mpg321 -q green.mp3")
+		except:
+			os.system("mpg321 -q green.mp3")
+			pass
 		logging()
 		ledSwitch = 0
 	if vattenbehov > 1:
@@ -133,19 +144,31 @@ def water_reading():
 		waterLevel = 50
 		lastWatered = (("Status update. The plant was last watered at " + strftime("%H:%M") + ", " + strftime("%A, %B %d" + ".")))
 		Thread(target = led_red_alert).start()
-		tts = gTTS(text="Code red. We have a code red. Watering protocols will now engage." , lang='en')
-		tts.save("red.mp3")
-		os.system("mpg321 -q red.mp3")
+		try:
+			tts = gTTS(text="Code red. We have a code red. Watering protocols will now engage." , lang='en')
+			tts.save("red.mp3")
+			os.system("mpg321 -q red.mp3")
+		except:
+			os.system("mpg321 -q red.mp3")
+			pass
 		time.sleep(1)
-		tts = gTTS(text="Alert! Water pump engaging." , lang='en')
-		tts.save("water.mp3")
-		os.system("mpg321 -q water.mp3")
+		try:
+			tts = gTTS(text="Alert! Water pump engaging." , lang='en')
+			tts.save("water.mp3")
+			os.system("mpg321 -q water.mp3")
+		except:
+			os.system("mpg321 -q water.mp3")
+			pass
 		water_pump()
 		time.sleep(10)
 		logging()
-		tts = gTTS(text="Moisture levels will now be re-tested by secondary systems." , lang='en')
-		tts.save("retest.mp3")
-		os.system("mpg321 -q retest.mp3")
+		try:
+			tts = gTTS(text="Moisture levels will now be re-tested by secondary systems." , lang='en')
+			tts.save("retest.mp3")
+			os.system("mpg321 -q retest.mp3")
+		except:
+			os.system("mpg321 -q retest.mp3")
+			pass
 		ledSwitch = 0
 		time.sleep(2)
 		water_reading()
@@ -269,7 +292,7 @@ def temp_humidity():
 	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 	global tweetMessage
 	tweetMessage = "Temperature: {0:0.1f} C  Humidity: {1:0.1f} %".format(temperature, humidity)
-	print(tweetMessage)
+	print("Tweeting: " + tweetMessage)
 	tweet()
 
 # Logging of statistics
@@ -286,10 +309,16 @@ def self_diagnostics():
 	Thread(target = led_rolling).start()
 	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 	temp = cpu.temperature
-	tts = gTTS(text="Self diagnostics. My CPU runs at {0:0.0f} degrees celsius. Environmental temperature is {1:0.0f} degrees celsius with a relative humidity of {2:0.0f} percent.".format(temp, temperature, humidity) , lang='en')
-	tts.save("diagnostics.mp3")
-	os.system("mpg321 -q diagnostics.mp3")
+	try:
+		tts = gTTS(text="Self diagnostics. My CPU runs at {0:0.0f} degrees celsius. Environmental temperature is {1:0.0f} degrees celsius with a relative humidity of {2:0.0f} percent.".format(temp, temperature, humidity) , lang='en')
+		tts.save("diagnostics.mp3")
+		os.system("mpg321 -q diagnostics.mp3")
+	except:
+		print("Internet error.")
+		internet_on()
+		pass
 	ledSwitch = 0
+	time.sleep(1)
 	internet_on()
 
 # Displaying last time plant was watered
@@ -305,70 +334,102 @@ def internet_on():
 		print("No internet connection.")
 		ledSwitch = 1
 		Thread(target = led_red_alert).start()
-		tts = gTTS(text="Alert! All communications are down. Alert! Systems running in emergency mode. Alert! Restoring communications, priority alpha." , lang='en')
-		tts.save("internet_off.mp3")
-		os.system("mpg321 -q internet_off.mp3")
+		try:
+			tts = gTTS(text="Alert! All communications are down. Alert! Systems running in emergency mode. Alert! Restoring communications, priority alpha." , lang='en')
+			tts.save("internet_off.mp3")
+			os.system("mpg321 -q internet_off.mp3")
+		except:
+			os.system("mpg321 -q internet_off.mp3")
+			pass
 		ledSwitch = 0
 		pass
 	else:
 		print("We have an internet connection.")
 		ledSwitch = 1
 		Thread(target = led_green_alert).start()
-		tts = gTTS(text="Code green! All communications systems working within normal parameters." , lang='en')
-		tts.save("internet_on.mp3")
-		os.system("mpg321 -q internet_on.mp3")
+		try:
+			tts = gTTS(text="Code green! All communications systems working within normal parameters." , lang='en')
+			tts.save("internet_on.mp3")
+			os.system("mpg321 -q internet_on.mp3")
+		except:
+			os.system("mpg321 -q internet_on.mp3")
+			pass
 		ledSwitch = 0
 
 # FILE UPLOADS
 
 # Init files upload
 def fileupload_init():
-	conf = yaml.load(open('credentials.yml'))
-	host = conf['user']['host']
-	port = conf['user']['port']
-	transport = paramiko.Transport((host, port))
+	try:
+		conf = yaml.load(open('credentials.yml'))
+		host = conf['user']['host']
+		port = conf['user']['port']
+		transport = paramiko.Transport((host, port))
 
-	password = conf['user']['password']
-	username = conf['user']['username']
-	transport.connect(username = username, password = password)
+		password = conf['user']['password']
+		username = conf['user']['username']
+		transport.connect(username = username, password = password)
 
-	sftp = paramiko.SFTPClient.from_transport(transport)
+		sftp = paramiko.SFTPClient.from_transport(transport)
 
-	sftp.chdir("/var/www/bloggmu/public/rum/blomma")
-	filepath = "stats.csv"
-	localpath = "/home/pi/kod/lovepot/stats.csv"
-	filepath2 = "index.html"
-	localpath2 = "/home/pi/kod/lovepot/index.html"
+		sftp.chdir("/var/www/bloggmu/public/rum/blomma")
+		filepath = "stats.csv"
+		localpath = "/home/pi/kod/empirepot/stats.csv"
+		filepath2 = "index.html"
+		localpath2 = "/home/pi/kod/empirepot/index.html"
 
-	sftp.put(localpath, filepath)
-	sftp.put(localpath2, filepath2)
+		sftp.put(localpath, filepath)
+		sftp.put(localpath2, filepath2)
 
-	sftp.close()
-	transport.close()
-	print("Init files have been uploaded.")
+		sftp.close()
+		transport.close()
+		print("Init files have been uploaded.")
+	except:
+		global ledSwitch
+		print("Warning. Initial files error uploading.")
+		ledSwitch = 1
+		Thread(target = led_red_alert).start()
+		tts = gTTS(text="Warning. Initial files error uploading." , lang='en')
+		tts.save("upload_init_error.mp3")
+		os.system("mpg321 -q upload_init_error.mp3")
+		ledSwitch = 0
+		internet_on()
+		pass
 
 # Stat file upload
 def fileupload_stats():
-	conf = yaml.load(open('credentials.yml'))
-	host = conf['user']['host']
-	port = conf['user']['port']
-	transport = paramiko.Transport((host, port))
+	try:
+		conf = yaml.load(open('credentials.yml'))
+		host = conf['user']['host']
+		port = conf['user']['port']
+		transport = paramiko.Transport((host, port))
 
-	password = conf['user']['password']
-	username = conf['user']['username']
-	transport.connect(username = username, password = password)
+		password = conf['user']['password']
+		username = conf['user']['username']
+		transport.connect(username = username, password = password)
 
-	sftp = paramiko.SFTPClient.from_transport(transport)
+		sftp = paramiko.SFTPClient.from_transport(transport)
 
-	sftp.chdir("/var/www/bloggmu/public/rum/blomma")
-	filepath = "stats.csv"
-	localpath = "/home/pi/kod/lovepot/stats.csv"
+		sftp.chdir("/var/www/bloggmu/public/rum/blomma")
+		filepath = "stats.csv"
+		localpath = "/home/pi/kod/empirepot/stats.csv"
 
-	sftp.put(localpath, filepath)
+		sftp.put(localpath, filepath)
 
-	sftp.close()
-	transport.close()
-	print("Stat file has been uploaded.")
+		sftp.close()
+		transport.close()
+		print("Stat file has been uploaded.")
+	except:
+		global ledSwitch
+		print("Warning. Stat file error uploading.")
+		ledSwitch = 1
+		Thread(target = led_red_alert).start()
+		tts = gTTS(text="Warning. Stat file error uploading." , lang='en')
+		tts.save("upload_error.mp3")
+		os.system("mpg321 -q upload_error.mp3")
+		ledSwitch = 0
+		internet_on()
+		pass
 
 # COMMUNICATIONS
 
@@ -388,8 +449,17 @@ def tweet():
 		api = tweepy.API(auth)
 
 		api.update_status(status=tweetMessage)
+		print("Tweet sent.")
 	except:
-		print("Error sending tweet.")
+		global ledSwitch
+		print("Warning. Twitter error. Unable to send tweet.")
+		ledSwitch = 1
+		Thread(target = led_red_alert).start()
+		tts = gTTS(text="Warning. Twitter error. Unable to send tweet." , lang='en')
+		tts.save("twitter_error.mp3")
+		os.system("mpg321 -q twitter_error.mp3")
+		ledSwitch = 0
+		internet_on()
 		pass
 
 def tweet_follow():
@@ -407,8 +477,17 @@ def tweet_follow():
 
 		for follower in tweepy.Cursor(api.followers).items():
 			follower.follow()
+		print("Twitter followers followed.")
 	except:
-		print("Error Twitter.")
+		global ledSwitch
+		print("Warning. Twitter error. Unable to follow twitter users.")
+		ledSwitch = 1
+		Thread(target = led_red_alert).start()
+		tts = gTTS(text="Warning. Twitter error. Unable to follow twitter users." , lang='en')
+		tts.save("twitter_follow_error.mp3")
+		os.system("mpg321 -q twitter_follow_error.mp3")
+		ledSwitch = 0
+		internet_on()
 		pass
 
 #################### MAIN PROGRRAM #################################
@@ -463,7 +542,6 @@ def Main():
 				GPIO.output(hygro_Power, False)
 			if val =="10":
 				logging()
-				fileupload_stats()
 			if val =="11":
 				fileupload_init()
 			if val =="12":
